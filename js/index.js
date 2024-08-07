bb8449ea6f57ed58ad63acd849ed0c7a1f0a4a2a
Elvisoj <163991614+Elvisoj@users.noreply.github.com>
2024-08-07 14:59:41 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+const lowerCase = "qwertyuiopasdfghjklzxcvbnm";
+const numbers = "0123456789";
+const symbols = "@£_&-+()#*:;!?,.€¥$¢^°={}∆¶×÷π√•|><";
+const allCharacters = upperCase + lowerCase + numbers + symbols;
+
+function generatePassword() {
+  let password = "";
+  /*
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+  */
+  
+  
+  while (length > password.length){
+    password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+  }
+  passwordBox.value = password;
+  
+}
+
+function copyPassword(){
+  passwordBox.select();
+  document.execCommand("copy");
+  alert("Passwird has been copied successfully!")
+}
